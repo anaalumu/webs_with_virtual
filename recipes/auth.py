74723fdc7bfbc9c2e82d69b00a4c07c32eabf395
ana.alum <100475297@alumnos.uc3m.es>
2023-12-08 12:47:10 +0100

    user login created and connected to database

---
+++ recipes/auth.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from . import db, bcrypt
+
+from . import model
+
+bp = Blueprint("auth", __name__)
+
+
+@bp.route("/signup")
+def signup():
+    return render_template("auth/signup.html")
+
+
+@bp.route("/signup", methods=["POST"])
+def signup_post():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    # Check that passwords are equal
+    if password != request.form.get("password_repeat"):
+        return redirect(url_for("auth.signup"))
+    # Check if the email is already at the database
+    query = db.select(model.User).where(model.User.email == email)
+    user = db.session.execute(query).scalar_one_or_none()
+    if user:
+        return redirect(url_for("auth.signup"))
+    password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+    new_user = model.User(email=email, name=username, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for("main.index"))
